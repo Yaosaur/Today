228c0685ca99951abdfd2b3b99922caf5dbfc1e4
Yao Jiang <yaojiang7595@gmail.com>
2022-09-06 21:36:05 -0400

    Fixed proj list loading on page load

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchProjects } from '../store/projects-slice';
 
-import { List, ListItemButton, ListItemText } from '@mui/material';
+import {
+  Stack,
+  Skeleton,
+  List,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material';
 
 function ProjectList() {
+  const dispatch = useDispatch();
   const nav = useNavigate();
   const projects = useSelector(state => state.projects.projects);
   const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(fetchProjects()).then(setIsLoading(false));
+  }, [dispatch]);
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
@@ -16,17 +30,25 @@ function ProjectList() {
 
   return (
     <List component='ul' aria-label='main mailbox folders'>
-      {projects.map((project, index) => {
-        return (
-          <ListItemButton
-            key={project._id}
-            selected={selectedIndex === index}
-            onClick={event => handleListItemClick(event, index)}
-          >
-            <ListItemText primary={project.title} />
-          </ListItemButton>
-        );
-      })}
+      {isLoading ? (
+        <Stack spacing={-3}>
+          <Skeleton variant='text' sx={{ fontSize: '4rem' }} />
+          <Skeleton variant='text' sx={{ fontSize: '4rem' }} />
+          <Skeleton variant='text' sx={{ fontSize: '4rem' }} />
+        </Stack>
+      ) : (
+        projects.map((project, index) => {
+          return (
+            <ListItemButton
+              key={project._id}
+              selected={selectedIndex === index}
+              onClick={event => handleListItemClick(event, index)}
+            >
+              <ListItemText primary={project.title} />
+            </ListItemButton>
+          );
+        })
+      )}
     </List>
   );
 }
