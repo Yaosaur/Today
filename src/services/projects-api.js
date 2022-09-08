0ad13dee04ac4148cd774bf813d29e6d34b3d798
Yao Jiang <yaojiang7595@gmail.com>
2022-09-08 14:50:28 -0400

    Ready for deployment

--- src/services/projects-api.js
+++ src/services/projects-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseURL = 'http://localhost:5000';
+let baseURL = 'https://today-project-backend.herokuapp.com';
 
 export const getProjects = () => {
   return axios.get(`${baseURL}/projects`);
--- src/services/tasks-api.js
+++ src/services/tasks-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseURL = 'http://localhost:5000';
+let baseURL = 'https://today-project-backend.herokuapp.com';
 
 export const createTask = (id, data) => {
   return axios.post(`${baseURL}/projects/${id}/task`, data);
