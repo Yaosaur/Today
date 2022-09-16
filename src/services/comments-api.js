526785b8c4e09410410df564e81b226e476241db
Yao Jiang <yaojiang7595@gmail.com>
2022-09-16 16:54:26 -0400

    Changed base for heroku

--- src/services/comments-api.js
+++ src/services/comments-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseURL = 'http://localhost:5000';
+let baseURL = 'https://today-project-backend.herokuapp.com';
 
 export const createComment = (taskId, data) => {
   return axios.post(`${baseURL}/comments/${taskId}`, data);
